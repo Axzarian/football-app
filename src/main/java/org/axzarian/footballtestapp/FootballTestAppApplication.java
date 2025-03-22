bea13fead04979fb78412277025c37461b87daa8
mikheyev <alexey.mikheyev@kaspi.kz>
2025-03-22 23:16:38 +0500

    Initial commit. Created project

---
+++ src/main/java/org/axzarian/footballtestapp/FootballTestAppApplication.java
@@ -0,0 +1,13 @@
+package org.axzarian.footballtestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballTestAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootballTestAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/axzarian/footballtestapp/FootballTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.axzarian.footballtestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballTestAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
