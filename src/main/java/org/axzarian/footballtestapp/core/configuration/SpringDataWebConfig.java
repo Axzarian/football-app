eaaf8ffe53606eaab799e43a2e900a80ee849fa6
mikheyev <alexey.mikheyev@kaspi.kz>
2025-10-05 12:38:17 +0500

    Back web config for pageable

---
+++ src/main/java/org/axzarian/footballtestapp/core/configuration/SpringDataWebConfig.java
@@ -0,0 +1,10 @@
+package org.axzarian.footballtestapp.core.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+@Configuration
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
+public class SpringDataWebConfig {
+    //  In order to return certain fields of page in response
+}
